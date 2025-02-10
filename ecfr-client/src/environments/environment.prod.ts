13ab008457cf17baa51f2580c00887a09f90d664
James Knippel <jamesknippel@gmail.com>
2025-02-09 19:48:49 -0500

    remove nginx proxy

--- ecfr-client/src/environments/environment.prod.ts
+++ ecfr-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: '/api',
+    apiUrl: 'https://ecfr-app-api.whitemeadow-0306cf91.eastus.azurecontainerapps.io/api/ecfr',
     ecfrBaseUrl: 'https://www.ecfr.gov'
 };
