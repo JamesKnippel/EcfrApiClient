6af414ad8ca10f0d04a6cb3f5428cd6efe5f009d
James Knippel <jamesknippel@gmail.com>
2025-02-09 20:42:22 -0500

    update nginx and environements, fix json refresh bug

--- ecfr-client/src/app/app.routes.ts
+++ ecfr-client/src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'api/ecfr/agencies',
+    redirectTo: 'app/agencies',
     pathMatch: 'full'
   },
   {
-    path: 'api/ecfr/agencies',
+    path: 'app/agencies',
     loadComponent: () => import('./components/agency-list/agency-list.component').then(m => m.AgencyListComponent)
   },
   {
-    path: 'api/ecfr/agencies/:slug',
+    path: 'app/agencies/:slug',
     loadComponent: () => import('./components/agency-detail/agency-detail.component').then(m => m.AgencyDetailComponent)
   },
   {
     path: '**',
-    redirectTo: 'api/ecfr/agencies'
+    redirectTo: 'app/agencies'
   }
 ];
