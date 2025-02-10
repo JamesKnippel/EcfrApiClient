ef66ef465e2122035ea1f58e2d2e27946dd20770
James Knippel <jamesknippel@gmail.com>
2025-02-10 09:21:05 -0500

    force api host and self redirect

--- ecfr-client/src/environments/environment.prod.ts
+++ ecfr-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ecfr-app-api.whitemeadow-0306cf91.eastus.azurecontainerapps.io/api',
+    apiUrl: '/api',  // Match the API service paths which expect /api/ecfr/...
     ecfrBaseUrl: 'https://www.ecfr.gov'
 };
