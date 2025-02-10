8757fb96af046a8f1f1cc603334b65413004a20a
James Knippel <jamesknippel@gmail.com>
2025-02-09 21:19:48 -0500

    strip from frontend, move to env

--- ecfr-client/src/environments/environment.ts
+++ ecfr-client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5217',  // This will be proxied via proxy.conf.json
+  apiUrl: 'http://localhost:5217/api',  // This will be proxied via proxy.conf.json
   ecfrBaseUrl: 'https://www.ecfr.gov'
 };
